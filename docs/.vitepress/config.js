97b94d36b87825b26841ee3d33dde55c9fb793d5
hankanon <hankanon@163.com>
2024-01-18 23:16:57 +0800

    md test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,27 +4,61 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "个人网站",
   description: "hankanon 的个人网站",
+  lastUpdated: true, // 最后更新时间
   // 主题配置选项
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    // 顶部导航栏
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
-      { text: '笔记', link: '/笔记/sass学习笔记' }
+      { text: '笔记', link: '/笔记/sass学习笔记' },
+      { text: 'test', link: 'test' }
     ],
-
+    // 侧边栏
     sidebar: [
       {
-        text: 'Examples',
+        text: '笔记',
+        collapsed: false,
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'sass学习笔记', link: '/sass学习笔记' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'aaaaaaa', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '读书',
+        items: [
+          { text: 'sass学习笔记', link: '/sass学习笔记' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'aaaaaaa', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '游戏',
+        items: [
+          { text: 'sass学习笔记', link: '/sass学习笔记' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'aaaaaaa', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '动漫',
+        items: [
+          { text: 'sass学习笔记', link: '/sass学习笔记' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'aaaaaaa', link: '/api-examples' }
         ]
       }
     ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    ],
+    // 页脚
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    },
+  },
 })
--- docs/markdown-examples.md
+++ docs/markdown-examples.md
@@ -1,3 +1,9 @@
+### Title <Badge type="info" text="default" />
+### Title <Badge type="tip" text="^1.9.0" />
+### Title <Badge type="warning" text="beta" />
+### Title <Badge type="danger" text="caution" />
+### Title <Badge type="info">custom element</Badge>
+
 # Markdown Extension Examples
 
 This page demonstrates some of the built-in markdown extensions provided by VitePress.
