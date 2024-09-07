73ef5746fea581d01f46723120da84d989f83dc5
hanxinnong <hanxn@missfresh.cn>
2024-09-07 16:20:42 +0800

    add baidu统计

--- docs/.vitepress/config/head.js
+++ docs/.vitepress/config/head.js
@@ -7,16 +7,30 @@ export default [
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+  // 本地测试
   [
     'script',
     {},
-    ` var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?22e67cdd4b97413abf41a775f89e50f6";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })();
+    `var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?6c1a25d25ff3b3cf8020279f94e027ef";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+    `,
+  ],
+  // 线上
+  [
+    'script',
+    {},
+    `var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?307234a84dc32232cd2eef10dad5821d";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
     `,
   ],
 ]
