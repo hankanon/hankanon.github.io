31dcff8f8a734180796a7e707769b952e11efab8
hanxinnong <hanxn@missfresh.cn>
2024-11-22 15:44:05 +0800

    添加 goTop

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <Layout>
+    <template #doc-footer-before>
+      <BackTop></BackTop>
+    </template>
+  </Layout>
+</template>
+
+<script setup>
+import DefaultTheme from "vitepress/theme";
+import BackTop from './components/backTop.vue';
+const { Layout } = DefaultTheme;
+</script>
